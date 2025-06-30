5e844819b0ed3150d8ee41cf43a7f1616ab7a76a
francescomaxim <francesco.maxim@icloud.com>
2025-07-01 02:49:52 +0300

    updated url v0.79.2

--- backend/authentication-service/AuthenticationManagement/src/main/java/com/example/AuthenticationManagement/AuthenticationManagementApplication.java
+++ backend/authentication-service/AuthenticationManagement/src/main/java/com/example/AuthenticationManagement/AuthenticationManagementApplication.java
@@ -20,7 +20,7 @@ public class AuthenticationManagementApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://adminapp-l6x3.vercel.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://adminapp-cttk.vercel.app/login"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
