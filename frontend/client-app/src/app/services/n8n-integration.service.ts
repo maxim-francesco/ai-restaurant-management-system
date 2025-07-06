184cff69fa4832e9a8a4e0a1c5b5988ea5727663
francescomaxim <francesco.maxim@icloud.com>
2025-07-06 20:09:46 +0300

    fixed

--- frontend/client-app/src/app/services/n8n-integration.service.ts
+++ frontend/client-app/src/app/services/n8n-integration.service.ts
@@ -10,16 +10,11 @@ export class N8nIntegrationService {
   private n8nWebhookUrl =
     'https://francescoomaxim.app.n8n.cloud/webhook-test/ea8dd505-0bae-437b-a80a-8220807934fa';
 
-  // Înlocuiește cu cheia secretă pe care ai definit-o în n8n
-  private apiKey = 'super-secret-key-123456789';
-
   constructor(private http: HttpClient) {}
 
   public triggerWorkflow(): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      // Aici pui header-ul de autentificare definit în n8n
-      'X-N8N-API-KEY': this.apiKey,
     });
 
     // Corpul cererii poate fi gol dacă nu trebuie să trimiți date
